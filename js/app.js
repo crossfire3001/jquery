8e3fdc32074fd880e40a2689a462e5ee18809d1d
crossfire3001 <obiwan2035@mail.ru>
2023-09-05 10:16:21 +0500

    using more jQuery functions - animate, fadeOut, hide

--- js/app.js
+++ js/app.js
@@ -37,4 +37,18 @@ $(document).ready(function () {
   $("input").on("click", function () {
     console.log($(this).text());
   });
+
+  $("h2").click(function () {
+    $("h2").hide();
+  });
+
+  $("h3").click(function () {
+    $("h3").fadeOut("slow", "linear", () => {
+      console.log("end");
+    });
+  });
+
+  $("h4").click(function () {
+    $("h4").animate({ fontSize: "70px", marginLeft: "+=50px" });
+  });
 });
